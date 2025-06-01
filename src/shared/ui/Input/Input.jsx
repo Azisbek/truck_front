147af505b5958e600ee05685089694573ecff1b1
DilSan00 <norms777666@gmail.com>
2025-06-01 20:58:18 +0600

    added eye for input passwrods

--- src/shared/ui/Input/Input.jsx
+++ src/shared/ui/Input/Input.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import { useState } from "react";
+import { IoEyeOffSharp, IoEyeSharp } from "react-icons/io5";
+import clsx from "clsx";
 import s from "./Input.module.css";
 
-export function Input({ ...props }) {
-  return <input {...props} className={s.input} />;
+export function Input({ type = "text", className, ...props }) {
+  const [isEye, setIsEye] = useState(false);
+
+  const isPassword = type === "password";
+  const actualType = isPassword ? (isEye ? "text" : "password") : type;
+
+  const toggleEye = () => setIsEye((prev) => !prev);
+
+  return (
+    <div className={s.container}>
+      <input
+        {...props}
+        type={actualType}
+        className={clsx(s.input, className)}
+      />
+      {isPassword && (
+        <span className={s.eye} onClick={toggleEye}>
+          {isEye ? <IoEyeSharp size={30} /> : <IoEyeOffSharp size={30} />}
+        </span>
+      )}
+    </div>
+  );
 }
